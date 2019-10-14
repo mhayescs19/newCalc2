f9ece7ad3416e5c514b7c91fec4f7acba58536be
mhayescs19 <mhayescs19@gmail.com>
2019-10-14 14:43:46 -0700

    Initial Commit

---
+++ newCalc/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  newCalc
+//
+//  Created by Hayes, Michael on 10/14/19.
+//  Copyright © 2019 Hayes, Michael. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
